c264979e21d22c3f121efe2a483a8f30ee023df2
Trinm104 <trinm3962@ut.edu.vn>
2025-12-06 10:23:08 +0700

    Update Bai01.java

--- Lab01/src/Bai01.java
+++ Lab01/src/Bai01.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Bai01 {
     public static void main(String[] args) {
-        System.out.println("Xin chgaof các bạn");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập tên sinh viên: ");
+        String name = scanner.nextLine();
+        System.out.println("Nhập điểm trung bình: ");
+        float gpa = scanner.nextFloat();
+        System.out.println("Tên sinh viên: " + name);
+        System.out.println("Điểm trung bình: " + gpa);
+        scanner.close();
 
     }
-}
